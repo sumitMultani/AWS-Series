dd956eec357fc78b37b463aaf644feeb928d3967
sumit <spsingh02497@gmail.com>
2020-07-31 16:50:06 +0530

    simple notification service

---
+++ simple-notification-service/src/test/java/com/example/simplenotifiacationservice/SimpleNotifiacationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplenotifiacationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNotifiacationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
